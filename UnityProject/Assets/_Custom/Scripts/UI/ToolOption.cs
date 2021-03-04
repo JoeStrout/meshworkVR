eedd1c2128fbb1021943e2fd21d9a399b9774ba8
Joe Strout <joe@strout.net>
2021-03-04 16:35:23 -0700

    Started on tools panel.  Working well enough, though not yet hooked up to actual tools.

---
+++ UnityProject/Assets/_Custom/Scripts/UI/ToolOption.cs
@@ -0,0 +1,63 @@
+﻿/*
+This script goes on each of the buttons that represent tools you can choose
+for either hand.  It works with a ToolsPanel component on the containing
+Canvas.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ToolOption : MonoBehaviour, IPointerDownHandler
+{
+	public GameObject leftHandIcon;
+	public GameObject rightHandIcon;
+	public Graphic background;
+	
+	public Color normalBgColor = new Color(0.5f, 0.5f, 0.5f);
+	public Color selectedBgColor = new Color(0.8f, 0.8f, 0.8f);
+	
+	public bool selectedLeft {
+		get {
+			return leftHandIcon.activeSelf;
+		}
+		set {
+			leftHandIcon.SetActive(value);
+			UpdateBackground();
+		}
+	}
+	
+	public bool selectedRight {
+		get {
+			return rightHandIcon.activeSelf;
+		}
+		set {
+			rightHandIcon.SetActive(value);
+			UpdateBackground();
+		}
+	}
+	
+	ToolsPanel owner;
+	
+	protected void Awake() {
+		owner = GetComponentInParent<ToolsPanel>();
+		Debug.Assert(owner != null);
+		selectedLeft = selectedRight = false;
+	}
+	
+	void UpdateBackground() {
+		background.color = (selectedLeft || selectedRight ? selectedBgColor : normalBgColor);
+	}
+	
+	public void OnPointerDown(PointerEventData p) {
+		//Debug.Log($"PointerDown on {gameObject.name}", gameObject);
+		var evtData = p as UnityEngine.XR.Interaction.Toolkit.UI.TrackedDeviceEventData;
+		if (evtData != null && evtData.interactor == owner.leftRayInteractor) {
+			owner.NoteClicked(this, true);
+		} else {
+			owner.NoteClicked(this, false);
+		}
+	}
+
+}
---
+++ UnityProject/Assets/_Custom/Scripts/UI/ToolsPanel.cs
@@ -0,0 +1,28 @@
+﻿/*
+This script manages the Tools panel, which shows all the different tools
+you can choose from for each hand.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ToolsPanel : MonoBehaviour
+{
+	public XRRayInteractor leftRayInteractor;
+	public XRRayInteractor rightRayInteractor;
+
+	List<ToolOption> options;
+	
+	protected void Awake() {
+		options = new List<ToolOption>(GetComponentsInChildren<ToolOption>());
+	}
+
+	public void NoteClicked(ToolOption option, bool leftHand) {
+		foreach (var opt in options) {
+			if (leftHand) opt.selectedLeft = (opt == option);
+			else opt.selectedRight = (opt == option);
+		}
+	}
+}
